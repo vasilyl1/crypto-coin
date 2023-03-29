cd99808ce2be92f41bc5bde004a7cef10309984a
Yaroslav Volvach <volvachya@gmail.com>
2023-03-29 10:42:09 -0400

    Update models

--- server/models/User.js
+++ server/models/User.js
@@ -8,10 +8,9 @@ const userSchema = new Schema({
   lastName: {type: String, required: true, trim: true},
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true, minlength: 5},
-  personalNews: [News]
+  personalNews: ['News']
 });
 
-// set up pre-save middleware to create password
 userSchema.pre('save', async function(next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
