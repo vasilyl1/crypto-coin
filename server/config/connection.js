8f4e40e59f686621e0bb8f2c121ce13af56c8481
vasilyl1 <likhovaido@gmail.com>
2023-03-29 09:54:24 -0400

    Mongo connection config change

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://cryptomanager:XQycHUltUDHh62FN@cluster0.owpr4r9.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/cryptobase', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
