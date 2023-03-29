9338094f613c2c2c0dce244ff679f83ad11ba250
vasilyl1 <likhovaido@gmail.com>
2023-03-29 12:42:32 -0400

    user and news schemas correction login and signups are operational

--- server/models/News.js
+++ server/models/News.js
@@ -1,12 +1,19 @@
-const mongoose = require('mongoose');
-
-const { Schema } = mongoose;
+const { Schema } = require('mongoose');
 
 const newsSchema = new Schema({
-  textContent: {type: String, required: true},
-  date: {type: String, required: true},
-  sources:{type: Schema.Types.ObjectId, ref: 'Source'},
+  textContent: {
+    type: String, 
+    required: true
+  },
+  date: {
+    type: String, 
+    equired: true
+  },
+  sources:{
+    type: Schema.Types.ObjectId, 
+    ref: 'Source'
+  },
 });
 
 
-module.exports =  mongoose.model('News', newsSchema);
+module.exports =  newsSchema;
