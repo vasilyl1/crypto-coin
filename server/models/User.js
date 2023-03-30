829e10498517be3c2b96624ccf1e54d37bc12837
Yaroslav Volvach <volvachya@gmail.com>
2023-03-30 15:26:48 -0400

    Update index.js

--- server/models/User.js
+++ server/models/User.js
@@ -37,7 +37,6 @@ userSchema.pre('save', async function(next) {
   next();
 });
 
-// compare the incoming password with the hashed password
 userSchema.methods.isCorrectPassword = async function(password) {
   return await bcrypt.compare(password, this.password);
 };
