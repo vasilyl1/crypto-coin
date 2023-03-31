223227bfe039db932733d6c572fdec907db588d7
Liguang Zhang <leezhang97@gmail.com>
2023-03-31 14:25:33 -0400

    fixed useQuery error not displaying data

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,33 +9,34 @@ const resolvers = {
         },
         getNews: async (parent, { subscription }) => {
             return await News.find({ subscription: subscription }).populate('source');
-        }
+        },
+
     },
 
-    Mutation: {
-        addUser: async (parent, { username, email, password }) => {
-            const user = await User.create({ username, email, password });
-            const token = signToken(user);
-            return { token, user };
+        Mutation: {
+            addUser: async (parent, { username, email, password }) => {
+                const user = await User.create({ username, email, password });
+                const token = signToken(user);
+                return { token, user };
+            },
+            login: async (parent, { email, password }) => {
+                const user = await User.findOne({ email });
+
+                if (!user) {
+                    throw new AuthenticationError('No user found with this email address');
+                }
+
+                const correctPw = await user.isCorrectPassword(password);
+
+                if (!correctPw) {
+                    throw new AuthenticationError('Incorrect credentials');
+                }
+
+                const token = signToken(user);
+
+                return { token, user };
+            },
         },
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
+    };
 
-            if (!user) {
-                throw new AuthenticationError('No user found with this email address');
-            }
-
-            const correctPw = await user.isCorrectPassword(password);
-
-            if (!correctPw) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
-
-            const token = signToken(user);
-
-            return { token, user };
-        },
-    },
-};
-
-module.exports = resolvers;
+    module.exports = resolvers;
